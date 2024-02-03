a9e47dafc660c3a81c2443fe2421a9662ece8324
jesh-exe <jrmurodiya@gmail.com>
2024-02-04 00:17:39 +0530

    Api Reborn

---
+++ JobVistaAPI/src/main/java/com/jobvista/entities/Address.java
@@ -0,0 +1,55 @@
+package com.jobvista.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "js_address_dtls")
+public class Address {
+    
+    @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name = "js_id")
+    @MapsId
+    private JobSeeker js;
+
+    @Size(max = 255)
+    @Column(name = "addr_lane1", nullable = false)
+    private String lane1;
+
+    @Size(max = 255)
+    @Column(name = "addr_lane2")
+    private String lane2;
+
+    @Size(max = 100)
+    
+    @Column(name = "addr_city", nullable = false, length = 100)
+    private String city;
+
+    @Size(max = 6)
+    @Column(name = "addr_pincode", nullable = false, length = 6)
+    private String pincode;
+
+    @Size(max = 100)
+    @Column(name = "addr_state", nullable = false, length = 100)
+    private String state;
+
+    @Size(max = 100)
+    @Column(name = "addr_country", nullable = false, length = 100)
+    private String country;
+
+}
