4dfceccb1030454764cff4e551dca0988f969616
jesh-exe <jrmurodiya@gmail.com>
2024-02-03 10:52:01 +0530

    Pushing Backend Structure Folder

---
+++ JobVistaAPI/src/main/java/com/jobvista/Application.java
@@ -0,0 +1,23 @@
+package com.jobvista;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Job-Vista-Full-Stack-Application
-This Repository contains original source code for the Full Stack Application - Job Vista, which was made while pursuing Post Graduation Diploma at CDAC - ACTS Pune
