1d2fe1055ac571b47afd242bad4c48d809606176
jesh-exe <jrmurodiya@gmail.com>
2024-02-04 23:08:01 +0530

    Few Change in Header

--- job-vista-react/src/components/Header.js
+++ job-vista-react/src/components/Header.js
@@ -10,22 +10,10 @@ import ScrollReveal from 'scrollreveal';
 export default function Header() {
 
   useEffect(() => {
-    ScrollReveal().reveal(".logoo", {
-      origin: "left",
-      duration: 1500,
-      distance: "200px",
-      scale: 1,
-    });
-    ScrollReveal().reveal(".middle", {
+    ScrollReveal().reveal(".logo", {
       origin: "top",
       duration: 1500,
       distance: "100px",
-      scale: 1,
-    });
-    ScrollReveal().reveal(".last", {
-      origin: "right",
-      duration: 1500,
-      distance: "200px",
       scale: 1,
     });
   }, [])
@@ -41,13 +29,13 @@ export default function Header() {
 
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto middle">
+            <Nav className="me-auto">
               <Nav.Link href="#home" className='ps-4 pe-4' >Home</Nav.Link>
               <Nav.Link href="#features" className='ps-4 pe-4' >Find a job</Nav.Link>
               <Nav.Link href="#pricing" className='ps-4 pe-4' >About</Nav.Link>
               <Nav.Link href="#contact" className='ps-4 pe-4' >Contact</Nav.Link>
             </Nav>
-            <div className='text-center last'>
+            <div className='text-center'>
               <button className="btn btn-success">Register</button>
               <button className="ms-4 btn btn-primary">Login</button>
             </div>
