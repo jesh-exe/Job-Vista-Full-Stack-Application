9543dac4c58434fc4a2caa24216ed04f9cd04113
jesh-exe <jrmurodiya@gmail.com>
2024-02-13 01:33:59 +0530

    Redux for Recruiter Login Created

---
+++ job-vista-react/src/redux/slices/Recruiter/RecruiterSlice.js
@@ -0,0 +1,57 @@
+import { createSlice,createSelector } from "@reduxjs/toolkit";
+
+// const job = {
+// 	"id": 0,
+// 	"recruiterName": "",
+// 	"jobCategory": "",
+// 	"experience": "",
+// 	"minimumEducation": "",
+// 	"description": "",
+// 	"role": "",
+// 	"responsibility": "",
+// 	"jobCity": "",
+// 	"workHours": 0,
+// 	"expectedSalary": "",
+// 	"bond": "",
+// 	"vacancies": 0,
+// 	"jobType": "",
+// }
+
+const initState = {
+	"recruiter": {
+		"firstName": "",
+		"username": "",
+		"email": "",
+		"companyName": "",
+		"companyUrl": "",
+		"jobs": [],
+		"totalApplicants": 0,
+		"activeJobs": 0
+	}
+}
+
+
+const loggedRecruiter = createSlice({
+	name: "LoggedRecruiter",
+	initialState: initState,
+	reducers: {
+		setRecruiterDetails: (state, action) => {
+			state.recruiter = action.payload;
+		}
+	}
+})
+
+export const getLoggedRecruiter = createSelector(
+	(state) => state.loggedRecruiter.recruiter,
+	(state) => state
+)
+
+export const getLoggedRecruiterJobs = createSelector(
+	(state) => state.loggedRecruiter.recruiter.jobs,
+	(state) => state
+)
+
+export const { setRecruiterDetails } = loggedRecruiter.actions;
+
+export default loggedRecruiter.reducer;
+
