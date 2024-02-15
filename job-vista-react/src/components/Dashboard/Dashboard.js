f71d84ba225df2deb0cf843d0099f6256c496fa4
jatinmitrani <jatinmitrani2002@gmail.com>
2024-02-15 14:47:32 +0530

    updating dashboard.js file from main

--- job-vista-react/src/components/Dashboard/Dashboard.js
+++ job-vista-react/src/components/Dashboard/Dashboard.js
@@ -1,14 +1,9 @@
 import React, { useEffect } from 'react'
 import Sidebar from './Sidebar'
-<<<<<<< HEAD
-import NewJob from './NewJob'
-
-=======
 import '../../css/Dashboard/Dashboard.css'
 import { useSelector } from 'react-redux'
 import { getLoggedRecruiter, getLoggedRecruiterJobs } from '../../redux/slices/Recruiter/RecruiterSlice'
 import { useNavigate } from 'react-router'
->>>>>>> origin/main
 function Dashboard() {
 
   //To Ensure that the Recruiter can use Dashboard
