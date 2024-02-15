5a2a5ec99b540129c785d3a2c19df55d5b0becdd
jesh-exe <jrmurodiya@gmail.com>
2024-02-16 01:47:57 +0530

    Minor Changes in UI of Dashboard and some tweaks added

--- job-vista-react/src/components/Dashboard/Dashboard.js
+++ job-vista-react/src/components/Dashboard/Dashboard.js
@@ -4,12 +4,20 @@ import '../../css/Dashboard/Dashboard.css'
 import { useSelector } from 'react-redux'
 import { getLoggedRecruiter, getLoggedRecruiterJobs } from '../../redux/slices/Recruiter/RecruiterSlice'
 import { useNavigate } from 'react-router'
+import { getLoggedJobSeeker } from '../../redux/slices/JobSeeker/JobSeekerSlice'
+import { toast } from 'react-toastify'
 function Dashboard() {
 
   //To Ensure that the Recruiter can use Dashboard
   const navigate = useNavigate();
   const recruiter = useSelector(getLoggedRecruiter);
-  if (recruiter.email === "" ) {
+  const jobSeeker = useSelector(getLoggedJobSeeker);
+  if(jobSeeker.email !== "")
+  {
+    toast.warn("You are not Authorized");
+    navigate("/");
+  }
+  else if (recruiter.email === "" ) {
     navigate("/login");
   }
 
